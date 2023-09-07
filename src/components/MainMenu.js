6eab5510a88da316e8699a2c8606af27c4a5da86
akshayachuthan <akshayacuthan@credenceanalytics.com>
2023-09-07 12:10:20 +0530

    menu item data update and some css changes

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -2,9 +2,36 @@ import Card from "../UI/Card"
 import MenuItem from "./MenuItem"
 
 const MenuData = [
-  {title:"Dosa", descritpion:"Plani dosa  with chutney and sambar", price: 22.40},
-  {title:"IDli", descritpion:"Idli  with chutney and sambar", price: 2.21},
-  {title:"Momos", descritpion:"steamed balls with stuffed vegies", price: 1.66}
+  {
+    title: "Momos",
+    description: "Steamed balls with stuffed veggies",
+    price: 1.66
+  },
+  {
+    title: "Burger",
+    description: "Delicious burger with cheese and fries",
+    price: 5.99
+  },
+  {
+    title: "Pizza",
+    description: "Margherita pizza with fresh basil and mozzarella",
+    price: 8.49
+  },
+  {
+    title: "Pasta Carbonara",
+    description: "Creamy pasta with bacon, eggs, and Parmesan cheese",
+    price: 9.99
+  },
+  {
+  title: "Salad Bowl",
+  description: "Fresh mixed greens with a variety of vegetables and choice of dressing",
+  price: 6.49
+  },
+  {
+    title: "Chicken Tacos",
+    description: "Grilled chicken tacos with salsa and guacamole",
+    price: 7.25
+  }
 ]
 
 const MainMenu = () => {
@@ -13,7 +40,7 @@ const MainMenu = () => {
     <Card>
       {
         MenuData.map((item,index) => (
-          <MenuItem key={index} title={item.title} description={item.descritpion} price={item.price}/>
+          <MenuItem key={index} title={item.title} description={item.description} price={item.price}/>
         ))
       }
     </Card>
